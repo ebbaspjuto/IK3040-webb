4d163eadae72ed03f8cfd58ac396b28298dfa3e5
ebbaspjuto <ebbaspj20@student.hh.se>
2021-05-17 22:12:55 +0200

    försök till att återanvända kod med javascript :-))

---
+++ JS/javascript.js
@@ -0,0 +1,18 @@
+console.log('Hej');
+
+// $(function(){
+//     $('#sidenav-placeholder').load('../HTML/nav-reuse.html');
+// });
+
+
+// event-listener to links in menu
+document.getElementById("sidenav").addEventListener("click", viewSidenav);
+
+function viewSidenav() {
+    alert('Heja mig');
+};
+
+
+
+
+
