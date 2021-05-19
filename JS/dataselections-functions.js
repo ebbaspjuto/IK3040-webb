e03c0630d0fcbb57da509beb06ac67bf387ca3c8
ebbaspjuto <ebbaspj20@student.hh.se>
2021-05-19 10:48:39 +0200

    ändrad id i main till class. Blev fel :)

--- JS/dataselections-functions.js
+++ JS/dataselections-functions.js
@@ -1,15 +1,26 @@
 // functions buttons
 
-console.log('Hallå')
+// all buttons
 const buttons = document.getElementsByTagName("button");
+
+// one choose one button
+const chooseButtons = document.getElementsByClassName("button-step-one")
+
 for(let i = 0; i < buttons.length; i++ ) {
 buttons[i].addEventListener("click", buttonStepOne);
 }
 
 function buttonStepOne(event){
-    alert('hello')
-    event.target.style.background
+    const button = event.target;
+    if (button.classList.contains("buttontarget")){
+        button.classList.remove("buttontarget")
+    } else {
+        if (button.classList.contains("button-step-one")){
+            for (let i=0; i<chooseButtons.length; i++){
+                chooseButtons[i].classList.remove("buttontarget");
+            }
+        }
+        button.classList.add("buttontarget");
+    }
 };
 
-
-//document.getElementById("sidenav").style.display="none";
