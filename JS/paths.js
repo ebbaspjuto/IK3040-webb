b9425799eb08f4a4e13ee24df53d535b9e5a6657
ebbaspjuto <ebbaspj20@student.hh.se>
2021-05-26 14:48:58 +0200

    .

---
+++ JS/paths.js
@@ -0,0 +1,17 @@
+// // from report > dateselection
+
+// // function makeSelection(){
+// //         window.location.href="dataselection.html"
+// // }
+
+// function makeSelection(){
+//         $('').load('../HTML/dataselection.html');
+// }
+
+// // from selection (popup or else) to process
+
+// function processSelections(){
+//         $('').load('../HTML/process.html');
+// }
+
+// // $('#popup-1-placeholder').load('../HTML/popups/preview.html');
