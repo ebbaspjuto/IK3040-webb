ae5bd5a7b15b7e4ac0e4a2db9ffa2dfa58c2861a
ebbaspjuto <ebbaspj20@student.hh.se>
2021-05-27 21:57:34 +0200

    fix meny

--- JS/nav-functions.js
+++ JS/nav-functions.js
@@ -2,7 +2,7 @@
 
 // event-listener to links in menu
 document.getElementById("burger").addEventListener("click", showSidenav);
-document.getElementById("sidenav").addEventListener("click", hideSidenav);
+document.getElementById("nav-link").addEventListener("click", hideSidenav);
 
 function hideSidenav() {
     document.getElementById("sidenav").style.display="none";
@@ -16,20 +16,42 @@ function showSidenav(){
 // function dropdown
 
 let dropdownContent = document.getElementsByClassName("dropdown-links");
+let arrowUp = document.getElementById('nav-arrow-up');
+let arrowDown = document.getElementById('nav-arrow-down');
 
 
 function displayDropdown(event){
     var childNodes = event.target.parentNode.childNodes;
+    var childs = event.target.childNodes;
+    var up = null;
+    var down = null;
+    for (let i = 0; i < childs.length; i++) {
+        if(childs[i].classList) {
+            if(childs[i].classList.contains('fa-chevron-up')) {
+                up = childs[i].classList;
+            } else if(childs[i].classList.contains('fa-chevron-down')) {
+                down = childs[i].classList;
+            }
+        }
+    }
+    if(up.contains('hide')) {
+        up.remove('hide');
+        down.add('hide')
+    } else {
+        down.remove('hide');
+        up.add('hide')
+    }
     for (let i = 0; i < childNodes.length; i++){
-        if (childNodes[i].classList && childNodes[i].classList.contains("dropdown-links")){
-           console.log(childNodes[i].style.display)
-            if(childNodes[i].style.display == "flex"){
-                childNodes[i].style.display = "none";
-            } else {
-                for (let j=0; j < dropdownContent.length; j++){
-                    dropdownContent[j].style.display = "none"
-                } 
-                childNodes[i].style.display = "flex";
+        if (childNodes[i].classList) {
+            if (childNodes[i].classList.contains("dropdown-links")) {
+                if(childNodes[i].style.display == "flex"){
+                    childNodes[i].style.display = "none";
+                } else {
+                    for (let j=0; j < dropdownContent.length; j++){
+                        dropdownContent[j].style.display = "none"
+                    } 
+                    childNodes[i].style.display = "flex";  
+                }
             }
         }
     }
--- JS/selection-functions.js
+++ JS/selection-functions.js
@@ -12,3 +12,21 @@ function yrke(){
     $('#selected-choice').load('../HTML/selection-choices/yrke.html');
 }
 
+// save
+
+let counter = 1; 
+let saveText = document.getElementById("savetext");
+let saveIcon = document.getElementById("saveicon")
+
+function inc(){
+    if (counter == 1){
+        saveIcon.classList.remove("saveRotate");
+    } if(counter < 10){
+        saveText.innerText = `Sparades: ${counter} sekunder sedan`;
+        counter = counter + 1;
+        } else {
+            saveIcon.classList.add("saveRotate");
+            counter = 0;
+        }
+} 
+setInterval(inc, 1000) 
